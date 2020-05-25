0c94e204e744c982815ba4ea1e683e6e92353dc5
rily0 <zhanghx_dy@163.com>
2020-05-25 21:05:23 +0800

    初始化代理项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="home">
-    <el-row type="flex" class="row-bg">
+    <div class="page-title">代理实例</div>
+    <div class="page-content">
+      <el-row :gutter="15" type="flex">
+        <el-col v-bind="cardLayout">
+          <div class="server-card">
+            <div class="card-title">实例A</div>
+            <div>本地服务</div>
+            <div>代理服务</div>
+          </div>
+        </el-col>
+        <el-col v-bind="cardLayout">
+          <div class="server-card">1</div>
+        </el-col>
+        <el-col v-bind="cardLayout">
+          <div class="server-card">1</div>
+        </el-col>
+        <el-col v-bind="cardLayout">
+          <div class="server-card new-proxy"><i class="iconfont icon-jia"/></div>
+        </el-col>
+      </el-row>
+    </div>
+    <!--<el-row type="flex" class="row-bg">
       <el-col :span="12" :offset="6">
         <el-form ref="form" :model="form" label-width="80px">
           <el-form-item label="访问路径">
@@ -16,13 +37,13 @@
           </el-form-item>
         </el-form>
       </el-col>
-    </el-row>
+    </el-row>-->
   </div>
 </template>
 
 <script>
-  // const express = require('express')
-  // const {createProxyMiddleware} = require('http-proxy-middleware')
+  import express from 'express'
+  import {createProxyMiddleware} from 'http-proxy-middleware'
 
   export default {
     name: 'home',
@@ -30,16 +51,19 @@
     data() {
       return {
         form: {},
-        app: null
+        app: null,
+        cardLayout: {
+          span: 6
+        }
       }
     },
     methods: {
       handleStart() {
-        console.log(require('express'))
         if (!this.app) {
-          // this.app = express()
-          // this.app.use('/api', createProxyMiddleware({target: 'http://www.example.org', changeOrigin: true}))
-          // this.app.listen(80)
+          this.app = express()
+          this.app.use('/api', createProxyMiddleware({target: 'http://www.example.org', changeOrigin: true}))
+          this.app.listen(80)
+          console.log('start')
         }
       },
       handleStop() {
@@ -49,6 +73,36 @@
 </script>
 <style lang="scss">
   .home {
-    padding: 20px 0;
+    padding: 20px;
+
+    .page-title {
+      font-size: 22px;
+      font-weight: 600;
+    }
+
+    .new-proxy {
+      justify-content: center;
+      cursor: pointer;
+      align-items: center;
+      flex-direction: row;
+
+      .iconfont {
+        font-size: 30px;
+      }
+    }
+
+    .server-card {
+      background-color: #eee;
+      border-radius: 3px;
+      box-shadow: 0 0 3px #bbb;
+      padding: 8px;
+      height: 120px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .card-title {
+      font-size: 20px;
+    }
   }
 </style>
---
+++ src/views/server.js
@@ -0,0 +1,4 @@
+import express from 'express'
+import {createProxyMiddleware} from 'http-proxy-middleware'
+
+export {express, createProxyMiddleware}
